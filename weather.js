99d5cc1be1cd8e48ae6731773ee96d042ab154b3
Aivirth <aivirth.work@gmail.com>
2018-02-12 17:45:02 +0100

    Structured Weather class

---
+++ weather.js
@@ -0,0 +1,23 @@
+'use strict';
+class Weather{
+    constructor(city, state){
+        this.apiKey = '9f2839ac7b5f5409';
+        this.city = city;
+        this.state = state;
+    }
+
+    //Fetch weather from API
+    async getWeather(){
+        const response = await fetch(`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json
+        `);
+
+        const responseData = await response.json();
+        return responseData.current_observation;
+    }
+
+    //Change weather location
+    changeLocation(city, state){
+        this.city = city;
+        this.state = state;
+    }
+}
