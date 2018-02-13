cac5be1f16aeef57b46a4bce2b1d2d7aa89de1bc
Aivirth <aivirth.work@gmail.com>
2018-02-13 16:53:27 +0100

    Added LocalStorage CRUD

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 "use strict";
+//Init Storage object
+const storage = new Storage;
+const weatherLocation = storage.getLocationData();
 //Init weather object
-const weather = new Weather('Roma', 'IT');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+//Init UI object
 const ui = new UI;
+
 
 //Get Weather on DOMLoad
 document.addEventListener('DOMContentLoaded', getWeather);
@@ -22,7 +27,8 @@ document.getElementById('w-change-btn').addEventListener('click', (e)=>{
     const city = document.getElementById('city').value;
     const state = document.getElementById('state').value;
 
-    weather.changeLocation('Miami', 'FL');
+    weather.changeLocation(city, state);
+    storage.setLocationData(city, state);
 
     //Get weather and display it
     getWeather();
