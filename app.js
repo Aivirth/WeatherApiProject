af0cb7ce20f83a4cb980d4eff7649a3d6add2750
Aivirth <aivirth.work@gmail.com>
2018-02-13 16:37:44 +0100

    Added Modal closure and location change

--- app.js
+++ app.js
@@ -14,3 +14,17 @@ function getWeather(){
     })
     .catch(error => console.log(error));
 }
+
+
+//Change Location event
+document.getElementById('w-change-btn').addEventListener('click', (e)=>{
+    
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    weather.changeLocation('Miami', 'FL');
+
+    //Get weather and display it
+    getWeather();
+    $('#locModal').modal('hide');
+});
